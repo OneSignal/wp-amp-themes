cefde16af0bcce084a84bbed12c9eaea6d6d7800
EkoAlex <alexandru@appticles.com>
2017-09-12 14:31:12 +0300

    Add extra description to push option

--- admin/pages/settings.php
+++ admin/pages/settings.php
@@ -114,7 +114,7 @@ $installed_themes = $wp_amp_themes_options->get_setting( 'installed_themes' );
 				<div class="spacer-10"></div>
 				<input type="hidden" name="wp_amp_themes_settings_push_enabled" id="wp_amp_themes_settings_push_enabled" value="<?php echo $push_notifications_enabled;?>" />
 				<input type="checkbox" name="wp_amp_themes_settings_push_enabled_check" id="wp_amp_themes_settings_push_enabled_check" value="0" <?php if ($push_notifications_enabled == 1) echo "checked" ;?> />
-				<label for ="wp_amp_themes_settings_push_enabled_check">Display push notifications on AMP pages</label>
+				<label for ="wp_amp_themes_settings_push_enabled_check">Display push notifications on AMP pages via the OneSignal plugin</label>
 				<div class="spacer-30"></div>
 				<a href="javascript:void(0)" id="wp_amp_themes_settings_send_btn" class="button button-primary button-large">Save</a>
 			</form>
